7f88a63024f0254e532b293e861dde45a07a41c2
Nikhil Srivastava <niksrc@gmail.com>
2016-04-23 20:11:05 +0530

    Simplifies the api and fixes race conditions

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 'use strict';
 var net = require('net');
-var socket = new net.Socket();
 
 module.exports = {
-	init: init,
-	get: get,
-	close: close
+	get: get
 };
 
-function init(opts) {
-	if (typeof opts.host !== 'string') {
-		throw new TypeError('Expected a string');
-	}
+function get(opts, text, callback) {
+	var socket = new net.Socket();
 
-	socket.connect(opts.port, opts.host);
-}
-
-function close() {
-	socket.destroy();
-}
-
-function get(text, callback) {
-	socket.on('connect', function () {
-		socket.on('data', function (response) {
-			var re = /<([A-Z]+?)>(.+?)<\/\1>/g;
-			var str = response.toString();
-			var m;
-			var result = {
-				LOCATION: [],
-				ORGANIZATION: [],
-				DATE: [],
-				MONEY: [],
-				PERSON: [],
-				PERCENT: [],
-				TIME: []
-			};
-
-			while ((m = re.exec(str)) !== null) {
-				if (m.index === re.lastIndex) {
-					re.lastIndex++;
-				}
-				result[m[1]].push(m[2]);
-			}
-
-			callback(result);
-		});
-
+	socket.connect(opts.port, opts.host, function () {
 		socket.write(text + '\n');
+	});
+
+	socket.on('data', function (data) {
+		var re = /<([A-Z]+?)>(.+?)<\/\1>/g;
+		var str = data.toString();
+
+		var res = {};
+		res.raw = str;
+
+		var m;
+		var entities = {
+			LOCATION: [],
+			ORGANIZATION: [],
+			DATE: [],
+			MONEY: [],
+			PERSON: [],
+			PERCENT: [],
+			TIME: []
+		};
+
+		var _parsed = [];
+		while ((m = re.exec(str)) !== null) {
+			if (m.index === re.lastIndex) {
+				re.lastIndex++;
+			}
+			_parsed.push(m);
+			entities[m[1]].push(m[2]);
+		}
+
+		res._parsed = _parsed;
+		res.entities = entities;
+		socket.destroy();
+		callback(undefined, res);
+	});
+
+	socket.on('error', function (err) {
+		callback(err, undefined);
 	});
 }
