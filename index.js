17703375d02c5120990c824a6e0c45f04bd4985c
Nikhil Srivastava <niksrc@gmail.com>
2018-06-02 14:31:56 +0530

    Bump packages

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
 'use strict';
-var net = require('net');
+const net = require('net');
 
 class NER {
-	////////////// options //////////////
-	// port NUMBER
-	// host string
-	// entities object of the entities
-	/////////////////////////////////////
+	/**
+	 * @param  {Object}
+	 * @property {String} host Host of NER server
+	 * @property {Number} port Port of NER server
+	 */
 	constructor(opts) {
-		this.opts = opts
+		this.opts = opts;
 	}
 
+	/**
+	 * @param  {String} text to be processed
+	 * @param  {Function} callback
+	 * @return {Object}
+	 * @property {Array} entities List of entitiy object
+	 */
 	get(text, callback) {
-		this.socket = new net.Socket()
-		
-		this.socket.connect(this.opts.port, this.opts.host,  ()  => {
+		this.socket = new net.Socket();
+
+		this.socket.connect(this.opts.port, this.opts.host, () => {
 			this.socket.setNoDelay(true);
-			this.socket.write(text.replace(/\r?\n|\r|\t/g, ' ') + '\n')
+			this.socket.write(text.replace(/\r?\n|\r|\t/g, ' ') + '\n');
 		});
 
-		this.socket.on('data', (data) => {
-			let re = /<([A-Za-z]+?)>(.+?)<\/\1>/g
-			let str = data.toString()
-			let res = {};
+		this.socket.on('data', data => {
+			const re = /<([A-Za-z]+?)>(.+?)<\/\1>/g;
+			const str = data.toString();
+			const res = {};
 			res.raw = str;
 
 			let m;
-			let _entities = {}
+			const _entities = {};
 
-			let _parsed = [];
+			const _parsed = [];
 			while ((m = re.exec(str)) !== null) {
 				if (m.index === re.lastIndex) {
 					re.lastIndex++;
 				}
 				_parsed.push(m);
-				if (!_entities[m[1]]) _entities[m[1]] = []
+				if (!_entities[m[1]]) {
+					_entities[m[1]] = [];
+				}
 				_entities[m[1]].push(m[2]);
 			}
 
@@ -44,10 +52,10 @@ class NER {
 			callback(undefined, res);
 		});
 
-		this.socket.on('error', (err) => {
+		this.socket.on('error', err => {
 			callback(err, undefined);
 		});
 	}
 }
 
-module.exports = NER
+module.exports = NER;
